8715bd3d7c3b83af2b7d3db32d979da2837c1997
Kameron Arceneaux <kameronarceneaux05@Gmail.com>
2023-12-14 23:57:09 -0600

    Initial Commit

---
+++ Memorize/ContentView.swift
@@ -0,0 +1,97 @@
+//
+//  ContentView.swift
+//  Memorize
+//
+//  Created by Kameron Arceneaux on 12/11/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    let emojis: [String] = ["👻", "🎃", "🕷️", "😈", "😈", "💀", "🕸️", "🧙", "🙀", "👹", "😱", "☠️", "🍭"]
+    @State var cardCount: Int = 4
+    
+    var body: some View {
+        VStack{
+            ScrollView {
+                cards
+            }
+            Spacer()
+            cardCountAdjusters
+        }
+        .padding()
+    }
+    
+    var cards: some View {
+        // Implicit return
+        LazyVGrid(columns: [GridItem(.adaptive(minimum: 120))]) {
+            ForEach(0..<cardCount, id: \.self){ index in
+                CardView(content: emojis[index])
+                    .aspectRatio(2/3, contentMode: .fit)
+            }
+        }
+        .foregroundColor(.orange)
+    }
+    
+    var cardCountAdjusters: some View {
+        HStack {
+            cardRemover
+            Spacer()
+            cardAdder
+        }
+        .imageScale(.large)
+        .font(.largeTitle)
+    }
+    
+    func cardCountAdjuster(by offset: Int, symbol: String) -> some View {
+        Button(action: {
+            cardCount += offset
+        }, label: {
+            Image(systemName: symbol)
+        })
+        .disabled(cardCount + offset < 1 || cardCount + offset > emojis.count)
+    }
+    
+    var cardRemover: some View {
+        cardCountAdjuster(by: -1, symbol: "rectangle.stack.badge.minus.fill")
+    }
+    
+    var cardAdder: some View {
+        cardCountAdjuster(by: 1, symbol: "rectangle.stack.badge.plus.fill")
+    }
+}
+
+struct CardView: View {
+    let content: String // remember this cant be changed
+    @State var isFaceUp = true //an example of a default value for a parameter
+    
+    
+    var body: some View {
+        ZStack {
+            let base = RoundedRectangle(cornerRadius: 12) //constant
+            Group {
+                base
+                    .fill(.white)
+                    .strokeBorder(lineWidth: 2)
+                Text(content).font(.largeTitle)
+            }
+            .opacity(isFaceUp ? 1: 0)
+            base.fill().opacity(isFaceUp ? 0: 1)
+        }
+        .onTapGesture {
+            isFaceUp.toggle()
+        }
+    }
+}
+
+
+
+
+
+
+
+
+
+#Preview {
+    ContentView()
+}
---
+++ Memorize/MemorizeApp.swift
@@ -0,0 +1,17 @@
+//
+//  MemorizeApp.swift
+//  Memorize
+//
+//  Created by Kameron Arceneaux on 12/11/23.
+//
+
+import SwiftUI
+
+@main
+struct MemorizeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
